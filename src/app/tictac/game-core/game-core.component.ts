745ff6e2cd2cc6521b5b54b655b30d27fba51fc2
Amine Mesbahi <mesbahiamine886@gmail.com>
2023-06-04 14:16:19 +0100

    add tictac game interface(board....) and some logical operation

--- src/app/tictac/game-core/game-core.component.ts
+++ src/app/tictac/game-core/game-core.component.ts
@@ -1,3 +1,5 @@
+import { Player } from '../modules/Player.model';
+import { MessageService } from '../services/message.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,17 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameCoreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageService:MessageService) { }
   board:string[][] = [
     ["","",""],
     ["","",""],
     ["","",""]
   ];
 
+  player1:Player = new Player();
+  player2:Player = new Player();
+
+  openSetNamePopUp:boolean=true;
+
+  
+  play(row:number,col:number){
+    this.board[row][col]="x";
+  }
 
   ngOnInit(): void {
-
+    this.messageService.initializeWebSocketConnection();
+    this.player1.name="Player1";
+    this.player2.name="Player2";
   }
+
+  start(name:String){
+    if(this.player1.name=="Player1"){
+      this.player1.name=name;
+      this.player1.shape="x";
+      this.player1.isYourTurn=true;
+    }else{
+      this.player2.name=name;
+      this.player2.shape="x";
+      this.player2.isYourTurn=true;
+    }
+    this.messageService.sendMessage(name);
+    this.openSetNamePopUp=false;
+  }
+
+
+
+
+
+
+
+
+
 
 
   // getRandomInt(min:number, max:number) : number{
--- src/app/tictac/tictac.module.ts
+++ src/app/tictac/tictac.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 
 import { GameCoreComponent } from './game-core/game-core.component';
+import { SetNamePopUpComponent } from './layouts/set-name-pop-up/set-name-pop-up.component';
 
 
 @NgModule({
   declarations: [
-    GameCoreComponent
+    GameCoreComponent,
+    SetNamePopUpComponent
   ],
   imports: [
-    CommonModule,    
+    CommonModule,
+    FormsModule
   ]
 })
 export class TictacModule { }
